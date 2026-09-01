2f35fa9cebe3da5dd941e2f08ec076501b498b18
anuj-tiwari-iphtech <anuj-tiwari@iphtechnologies.com>
2026-09-01 16:48:55 +0530

    Update component name

--- src/components/AlertModal/AlertContext.jsx
+++ src/components/AlertModal/AlertContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import AlertModal from "./AlertMOdal";
+import AlertModal from "./AlertModal";
 
 const AlertContext = createContext();
 
